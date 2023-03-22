35cf606ab441e4fc389ce2b14d42a440ddaa81d7
ag88 <ag88@github.com>
2023-03-22 21:58:20 +0800

    Added authentication, command line arguments
    - Added (BASIC) authentication
    - Added command line arguments, port, host, user,
      path, password can be specified

--- README.md
+++ README.md
@@ -1,31 +1,68 @@
-A WebDAV server based on Apache Tomcat's Webdav servlet and embedded tomcat server
+A WebDAV server based on Apache Tomcat's Webdav servlet and embedded Tomcat server
 ---
 
-This is a WebDAV server based on Apache Tomcat's WebDAV servlet and embedded tomcat server
+This is a WebDAV server based on Apache Tomcat's WebDAV servlet and embedded Tomcat server
 
-Current status: alpha/scratch/trial
+Current status: alpha/test
 
-- the port is patched in the source code 8082, runs on localhost
-- it serves the current working directory in which the app is started
-- no authentication etc
+## Feature list
+- It runs on http://localhost:8080/webdav
+- It serves the current working directory in which the app is started
+- Added (BASIC) authentication
+- Added command line aguments so that various parameters can be changed:
+- It requires a folder 'work' for the embedded Tomcat instance, if the folder isn't present,
+it is created.
 
-![screenshot in a browser](https://github.com/ag88/embtomcatwebdav/raw/main/web/screenshot.jpg "Screen shot")
+![screenshot in a browser](web/screenshot.jpg "Screen shot")
 
 # Build
-
+```
 mvn package
-
+```
 # Run
 
 ```
-java -jar target/webdav-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+java -jar webdav-0.1.0.jar
 ```
+Note that if you build from source the file name is webdav-0.1.0-jar-with-dependencies.jar, in target/ folder.
+
+## usage 
+
+```
+java -jar webdav-0.1.0.jar -h
+
+usage: webdav-0.1.0
+ -h,--help                help
+ -H,--host <hostname>     set host
+ -p,--port <port>         set port
+ -P,--path <path>         set path, default current working dir
+ -R,--realm <realm>       set realm, default 'simple'
+ -u,--user <username>     set user
+ -w,--passwd <password>   set password, omit this and it would prompt for
+                          it if -u is specified
+```
+
+note that the app can be run without specifying arguments.
 
 requires JDK >= 1.8
 
-Note that on starting, it creates a folder "work" in the current directory.
-and serves the current directory on webdav at http://localhost:8082/webdav
+Note that on starting, it creates a folder "work" in the current directory. This is needed for the embedded Tomcat instance to operate.
+And it serves the current directory on webdav at http://localhost:8080/webdav
 
-On running, point the web browser to http://localhost:8082/webdav, you should see the directory listing of your current work directory. For more functionality, it requires a WebDAV client to interact with the WebDAV server
+On running, point the web browser to http://localhost:8080/webdav, you should see the directory listing of your current work directory. For more functionality, it requires a WebDAV client to interact with the WebDAV server
 
+To enable authentication, specify a userid using -u option and password would be prompted if the -w option is not specified.
 
+# Attributions
+
+The server is really [Apache Tomcat https://tomcat.apache.org/](https://tomcat.apache.org/).
+One of the most widely used open-source implementation of the Jakarta Servlet, JSP technologies.
+
+## Origin
+
+github repository: 
+[https://github.com/ag88/embtomcatwebdav](https://github.com/ag88/embtomcatwebdav)
+
+I've made efforts to tailor the WebDAV servlet so that it works as a standalone app. 
+Developing this has taken quite some effort mainly in adding features, getting it to work and rather extensive tests. if you use this app and found it useful, i'd really appreciate it if you could support my efforts [![Donate](web/donorbox.png)](https://donorbox.org/embedded-tomcat-webdav-server).
+you could also help simply starring this repository ;)
--- src/main/java/com/embtomcat/webdav/App.java
+++ src/main/java/com/embtomcat/webdav/App.java
@@ -1,27 +1,50 @@
 package com.embtomcat.webdav;
 
-import javax.servlet.ServletConfig;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Realm;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.servlets.WebdavServlet;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.tomcat.util.descriptor.web.ApplicationParameter;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.juli.logging.Log;
+import org.apache.juli.logging.LogFactory;
+import org.apache.tomcat.util.descriptor.web.LoginConfig;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 
 public class App 
 {
 	
-	final int port = 8082;
-	final String host = "localhost";
+	Log log = LogFactory.getLog(App.class);
 	
 	Tomcat tomcat; 
 	
+	int port = 8080;
+	String shost = "localhost";
+	String path = System.getProperty("user.dir");
+	String user = null;
+	String passwd = null;
+	String realm = "simple";
+
 	public App() {
 		tomcat = new Tomcat();
-		tomcat.setPort(port);
-		tomcat.setHostname(host);
-		tomcat.setBaseDir(System.getProperty("user.dir"));
 		
 		Thread hook = new Thread() {
 			@Override
@@ -30,7 +53,7 @@ public class App
 					tomcat.stop();
 					tomcat.destroy();
 				} catch (LifecycleException e) {
-					e.printStackTrace();
+					log.error(e.getMessage(), e);
 				}				
 			}			
 		};			
@@ -39,25 +62,149 @@ public class App
 	
 	
 	public void run(String[] args) {
+		parseargs(args);
+		runserver();
+	}
+	
+	private void runserver() {
 		try {
-			Context context = tomcat.addContext("", System.getProperty("user.dir"));
+			tomcat.setPort(port);		
+			tomcat.setHostname(shost);
+			tomcat.setBaseDir(path);
+
+			//Context context = tomcat.addContext("", path);
+			Context context = tomcat.addWebapp("", path);
+			log.info(String.format("serving path: %s", path));
+
+			if (user != null) {
+				tomcat.addUser(user, passwd);
+				tomcat.addRole(user, "user");
+				Realm realm = tomcat.getEngine().getRealm();
+				context.setRealm(realm);
+				LoginConfig lconf = new LoginConfig();
+				// NONE, BASIC, DIGEST, FORM, or CLIENT-CERT.
+				lconf.setAuthMethod("BASIC");
+				lconf.setRealmName("Simple");
+				context.setLoginConfig(lconf);
+				SecurityConstraint secconstr = new SecurityConstraint();
+				secconstr.addAuthRole("user");
+				secconstr.setAuthConstraint(true);
+				SecurityCollection sc = new SecurityCollection();
+				sc.addPattern("/*");
+				secconstr.addCollection(sc);
+				context.addConstraint(secconstr);
+			}			
 			
 			WebdavServlet webdav = new WebdavServlet();
 			Wrapper ws = Tomcat.addServlet(context, "webdav", webdav);			
 			ws.addInitParameter("debug", "0");
 			ws.addInitParameter("listings", "true");
+			ws.addInitParameter("sortListings", "true");
 			ws.addInitParameter("readonly", "false");
-			ws.addInitParameter("allowSpecialPaths", "true");
+			ws.addInitParameter("allowSpecialPaths", "true");			
 
 			context.addServletMappingDecoded("/webdav/*", "webdav");
 
 			tomcat.start();
 			tomcat.getServer().await();
 		} catch (LifecycleException e) {
-			e.printStackTrace();
-		}
+			log.error(e.getMessage(), e);
+		}		
 	}
 	
+	private void parseargs(String[] args) {
+		Options options = new Options();
+		options.addOption(Option.builder("h").longOpt("help").desc("help").build());		
+		options.addOption(Option.builder("H").longOpt("host").desc("set host")
+				.hasArg().argName("hostname").build());
+		options.addOption(Option.builder("p").longOpt("port").desc("set port")
+				.hasArg().argName("port").build());
+		options.addOption(Option.builder("P").longOpt("path")
+				.desc("set path, default current working dir")
+				.hasArg().argName("path").build());
+		options.addOption(Option.builder("u").longOpt("user")
+				.desc("set user")
+				.hasArg().argName("username").build());
+		options.addOption(Option.builder("w").longOpt("passwd")
+				.desc("set password, you may omit this, it would prompt for it if -u is specified")
+				.hasArg().argName("password").build());
+		
+		try {
+			CommandLineParser parser = new DefaultParser();
+			CommandLine cmd = parser.parse(options, args);
+						
+			if (cmd.hasOption("host")) {
+				shost = cmd.getOptionValue("host");
+			} 
+			
+			if (cmd.hasOption("port")) {
+				try {
+					port = Integer.parseInt(cmd.getOptionValue("port"));
+				} catch (NumberFormatException e) {
+					log.warn("invalid port: ".concat(cmd.getOptionValue("port"))
+							.concat(", using default 8080 instead"));
+					port = 8080;
+				}
+			} 
+			
+			if (cmd.hasOption("path")) {
+				String p = cmd.getOptionValue("path");
+				path = new File(p).getAbsolutePath();
+			} 
+			
+			if (cmd.hasOption("user")) {
+				user = cmd.getOptionValue("user");
+			} 
+			
+			if (cmd.hasOption("passwd")) {
+				passwd = cmd.getOptionValue("passwd");
+			}
+
+			if (user != null && passwd == null) {
+				log.info(String.format("enter password for %s:", user));
+				Scanner input = new Scanner(System.in);
+				passwd = input.nextLine();
+				input.close();
+			}
+			
+			if(cmd.hasOption("help")) {
+				HelpFormatter formatter = new HelpFormatter();
+				Map<String, String> mkv = readManifest();
+				String name = mkv.get("artifactId")
+						.concat("-").concat(mkv.get("version"));
+				formatter.printHelp(name, options);
+				System.exit(0);
+			}
+			
+		} catch (ParseException e) {
+			log.error(e.getMessage(),e);
+		}
+		
+	}	
+	
+	
+	private Map<String, String> readManifest() {
+		TreeMap<String, String> mret = new TreeMap<String, String>();
+		try {
+
+			Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
+			while (resources.hasMoreElements()) {
+				Manifest manifest = new Manifest(resources.nextElement().openStream());
+
+				Attributes a = manifest.getMainAttributes();
+				for (Object o : a.keySet()) {
+					String k = o.toString();
+					String v = a.get(o).toString();
+					mret.put(k, v);
+				}
+
+				return mret;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}	
     public static void main(String[] args)  {
         App app = new App();
         app.run(args);
