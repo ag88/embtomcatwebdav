7655c3eb6f358ad73d578f034126738b8ef69ffb
ag88 <ag88@github.com>
2023-03-23 16:23:37 +0800

    drop JSP container, slimmer binary, ...
    - dropped JSP container, makes for a quite a bit smaller binnary
    - added option to change basedir - tomcat's work dir
    - added 'quiet' option, mute most logs
    - added script/batch files to run server which mutes
      illegal reflective access operation warnings

---
+++ run.bat
@@ -0,0 +1,9 @@
+@echo off
+rem mute illegal reflective access operation warnings >= jdk 9
+set "JDK_JAVA_OPTIONS=%JDK_JAVA_OPTIONS% --add-opens=java.base/java.lang=ALL-UNNAMED"
+set "JDK_JAVA_OPTIONS=%JDK_JAVA_OPTIONS% --add-opens=java.base/java.io=ALL-UNNAMED"
+set "JDK_JAVA_OPTIONS=%JDK_JAVA_OPTIONS% --add-opens=java.base/java.util=ALL-UNNAMED"
+set "JDK_JAVA_OPTIONS=%JDK_JAVA_OPTIONS% --add-opens=java.base/java.util.concurrent=ALL-UNNAMED"
+set "JDK_JAVA_OPTIONS=%JDK_JAVA_OPTIONS% --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED"
+
+java %JDK_JAVA_OPTIONS%  -jar target/webdav-0.1.1-jar-with-dependencies.jar %*
--- src/main/java/com/embtomcat/webdav/App.java
+++ src/main/java/com/embtomcat/webdav/App.java
@@ -3,6 +3,7 @@ package com.embtomcat.webdav;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Enumeration;
 import java.util.Map;
 import java.util.Scanner;
@@ -11,9 +12,12 @@ import java.util.jar.Attributes;
 import java.util.jar.Manifest;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.Host;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.Realm;
+import org.apache.catalina.Valve;
 import org.apache.catalina.Wrapper;
+import org.apache.catalina.authenticator.BasicAuthenticator;
 import org.apache.catalina.servlets.WebdavServlet;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.commons.cli.CommandLine;
@@ -39,28 +43,15 @@ public class App
 	int port = 8080;
 	String shost = "localhost";
 	String path = System.getProperty("user.dir");
+	String basedir = null;
 	String user = null;
 	String passwd = null;
-	String realm = "simple";
+	String realm = "Simple";
+	boolean quiet = false;
 
 	public App() {
-		tomcat = new Tomcat();
-		
-		Thread hook = new Thread() {
-			@Override
-			public void run() {
-				try {
-					tomcat.stop();
-					tomcat.destroy();
-				} catch (LifecycleException e) {
-					log.error(e.getMessage(), e);
-				}				
-			}			
-		};			
-		Runtime.getRuntime().addShutdownHook(hook);
 	}
-	
-	
+		
 	public void run(String[] args) {
 		parseargs(args);
 		runserver();
@@ -68,13 +59,38 @@ public class App
 	
 	private void runserver() {
 		try {
-			tomcat.setPort(port);		
+			if(basedir == null)
+				basedir = System.getProperty("user.dir")
+				.concat("/tomcat.")
+				.concat(Integer.toString(port));
+			if(!quiet)
+				log.info(String.format("tomcat basedir: %s", basedir));
+			
+			tomcat = new Tomcat();			
+			tomcat.setBaseDir(basedir);
+			tomcat.setSilent(quiet);
+			tomcat.setPort(port);
 			tomcat.setHostname(shost);
-			tomcat.setBaseDir(path);
-
-			//Context context = tomcat.addContext("", path);
-			Context context = tomcat.addWebapp("", path);
-			log.info(String.format("serving path: %s", path));
+			
+			Thread hook = new Thread() {
+				@Override
+				public void run() {
+					try {
+						tomcat.stop();
+						tomcat.destroy();
+					} catch (LifecycleException e) {
+						log.error(e.getMessage(), e);
+					}				
+				}			
+			};			
+			Runtime.getRuntime().addShutdownHook(hook);
+			
+			Context context = tomcat.addContext("", path);
+			tomcat.setAddDefaultWebXmlToWebapp(false);
+			Tomcat.addDefaultMimeTypeMappings(context);
+			
+			if(!quiet)
+				log.info(String.format("serving path: %s", path));
 
 			if (user != null) {
 				tomcat.addUser(user, passwd);
@@ -85,14 +101,16 @@ public class App
 				// NONE, BASIC, DIGEST, FORM, or CLIENT-CERT.
 				lconf.setAuthMethod("BASIC");
 				lconf.setRealmName("Simple");
+				lconf.setCharset(Charset.forName("UTF-8"));
 				context.setLoginConfig(lconf);
+				context.getPipeline().addValve(new BasicAuthenticator());
 				SecurityConstraint secconstr = new SecurityConstraint();
 				secconstr.addAuthRole("user");
 				secconstr.setAuthConstraint(true);
 				SecurityCollection sc = new SecurityCollection();
 				sc.addPattern("/*");
 				secconstr.addCollection(sc);
-				context.addConstraint(secconstr);
+				context.addConstraint(secconstr);				
 			}			
 			
 			WebdavServlet webdav = new WebdavServlet();
@@ -104,6 +122,7 @@ public class App
 			ws.addInitParameter("allowSpecialPaths", "true");			
 
 			context.addServletMappingDecoded("/webdav/*", "webdav");
+			context.setSessionTimeout(30);
 
 			tomcat.start();
 			tomcat.getServer().await();
@@ -122,12 +141,16 @@ public class App
 		options.addOption(Option.builder("P").longOpt("path")
 				.desc("set path, default current working dir")
 				.hasArg().argName("path").build());
+		options.addOption(Option.builder("b").longOpt("basedir")
+				.desc("set basedir, a work folder for tomcat, default [current working dir]/tomcat.port")
+				.hasArg().argName("path").build());
 		options.addOption(Option.builder("u").longOpt("user")
 				.desc("set user")
 				.hasArg().argName("username").build());
 		options.addOption(Option.builder("w").longOpt("passwd")
 				.desc("set password, you may omit this, it would prompt for it if -u is specified")
 				.hasArg().argName("password").build());
+		options.addOption(Option.builder("q").longOpt("quiet").desc("mute (most) logs").build());
 		
 		try {
 			CommandLineParser parser = new DefaultParser();
@@ -152,6 +175,11 @@ public class App
 				path = new File(p).getAbsolutePath();
 			} 
 			
+			if (cmd.hasOption("basedir")) {
+				String p = cmd.getOptionValue("basedir");
+				basedir = new File(p).getAbsolutePath();
+			}
+			
 			if (cmd.hasOption("user")) {
 				user = cmd.getOptionValue("user");
 			} 
@@ -165,6 +193,10 @@ public class App
 				Scanner input = new Scanner(System.in);
 				passwd = input.nextLine();
 				input.close();
+			}
+			
+			if(cmd.hasOption("quiet")) {
+				quiet = true;
 			}
 			
 			if(cmd.hasOption("help")) {
