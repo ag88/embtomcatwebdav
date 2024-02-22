03ba3efd779481e1f1aeec1297d21427901e3aa8
ag88 <ag88@github.com>
2024-02-22 20:04:31 +0800

    v0.8.2 improvements, fixes for config file generation, upload servlet default
    - use upload servlet by default
    - improvements, fixes for config file generation,
      config file generation now adds description entries and is sorted
    - improved help documentation / description
    - published to maven central

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.base/java.io=ALL-UNNAMED"
 JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.base/java.util=ALL-UNNAMED"
 JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.base/java.util.concurrent=ALL-UNNAMED"
 JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED"
-java $JDK_JAVA_OPTIONS -jar target/embtomcatwebdav-0.8.1.jar $*
+java $JDK_JAVA_OPTIONS -jar target/embtomcatwebdav-0.8.2.jar $*
